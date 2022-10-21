46e1489770f4fcca26d54eb512c157e7396a30bc
Pollux05 <justus@nj-racing.de>
2022-10-21 16:12:05 +0200

    first final try with graphs

--- MA4_Files/MA4_2.py
+++ MA4_Files/MA4_2.py
@@ -1,6 +1,22 @@
 #!/usr/bin/env python3
 
 from person import Person
+from numba import njit
+import matplotlib.pyplot as plt
+from time import perf_counter as pc
+
+def fib_py(n):
+	if n<=1:
+		return 1
+	else:
+		return fib_py(n-1)+fib_py(n-2)
+
+@njit
+def fib_numba(n):
+	if n<=1:
+		return 1
+	else:
+		return fib_numba(n-1)+fib_numba(n-2)
 
 def main():
 	f = Person(5)
@@ -9,7 +25,85 @@ def main():
 	print(f.get())
 	f.fib()
 	print(f.fib())
-	
+
+# Plot for 20-30
+	n_py = []
+	time_py = []
+	i=1
+	for i in range (11):
+		i+=10
+		start = pc()
+		fib_py(i)
+		end = pc()
+		time_py.append(end-start)
+		n_py.append(i)
+
+	n_numba = []
+	time_numba = []
+	i=1
+	for i in range (11):
+		i+=10
+		start = pc()
+		fib_numba(i)
+		end = pc()
+		time_numba.append(end-start)
+		n_numba.append(i)
+
+	plt.scatter(n_py, time_py, c="blue", label="fib_py(n)")
+	plt.scatter(n_numba, time_numba, c="red", label = "fib_numba(n)")
+	plt.xlabel("n")
+	plt.legend()
+	plt.ylabel("time in seconds")
+	#plt.savefig("Fib_20-30_time_plot.png", dpi=300)
+
+# Plot for 30-45
+	n_py = []
+	time_py = []
+	i=1
+	for i in range(16):
+		i+=30
+		start = pc()
+		fib_py(i)
+		end = pc()
+		time_py.append(end-start)
+		n_py.append(i)
+
+	n_numba = []
+	time_numba = []
+	i=1
+	for i in range(16):
+		i+=30
+		start = pc()
+		fib_numba(i)
+		end = pc()
+		time_numba.append(end-start)
+		n_numba.append(i)
+
+	n_cpp = []
+	time_cpp = []
+	i = 1
+	for i in range(16):
+		i+=30
+		f=Person(i)
+		start = pc()
+		f.fib()
+		end = pc()
+		time_cpp.append(end-start)
+		n_cpp.append(i)
+
+	plt.scatter(n_py, time_py, c = "blue", label="fib_py(n)")
+	plt.scatter(n_numba, time_numba, c = "red", label = "fib_numba(n)")
+	plt.scatter(n_cpp, time_cpp, c = "green", label = "f=Person(n);f.fib()")
+	plt.title("Time to calculate Fibonacci of n")
+	plt.xlabel("n")
+	plt.legend()
+	plt.ylabel("time in seconds")
+	plt.savefig("Fib_30-45_time_plot.png", dpi=300)
+
+
+
+
+
 if __name__ == '__main__':
 	main()
 	
